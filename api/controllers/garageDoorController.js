03e8fced0515737153935318469de815c5d5aeee
matgenois <7738711+matgenois@users.noreply.github.com>
2018-06-27 13:23:23 -0400

    Update garageDoorController.js

--- api/controllers/garageDoorController.js
+++ api/controllers/garageDoorController.js
@@ -7,16 +7,15 @@ const RELAY_GPIO_ID = 7;
 
 rpio.open(RELAY_GPIO_ID, rpio.INPUT, rpio.PULL_DOWN);
 
-exports.readState = function (req, res) {
-    console.log('readState');
+function readDoorState() {
     rpio.open(SENSOR_GPIO_ID, rpio.INPUT);
-    res.json({sensor:rpio.read(SENSOR_GPIO_ID)});
-};
+    var state = rpio.read(SENSOR_GPIO_ID);
+    rpio.close(SENSOR_GPIO_ID);
+    return state;
+}
 
-exports.toggle = function (req, res) {
-  console.log('Toggling door!');
-  rpio.open(SENSOR_GPIO_ID, rpio.INPUT);
-  var state = rpio.read(SENSOR_GPIO_ID);
+function toggle() {
+  var state = readDoorState();
   console.log('Current GPIO state is : ', state);
 
   rpio.open(RELAY_GPIO_ID, rpio.OUTPUT, rpio.HIGH);
@@ -25,6 +24,36 @@ exports.toggle = function (req, res) {
   rpio.write(RELAY_GPIO_ID, rpio.HIGH);
 
   rpio.msleep(200);
+  rpio.close(RELAY_GPIO_ID, rpio.PIN_PRESERVE);
+}
+
+exports.readState = function (req, res) {
+    console.log('readState');
+    res.json({sensor:readDoorState()});
+};
+
+exports.toggle = function (req, res) {
+  console.log('Toggling door!');
+  toggle();
   res.json({status:"success"});
 };
 
+exports.open = function(req, res) {
+  var state = readDoorState();
+  if (state) {
+      res.json({status:"already open"});
+  } else {
+      toggle();
+      res.json({status:"success"});
+  }
+};
+
+exports.close = function(req, res) {
+  var state = readDoorState();
+  if (state) {
+      res.json({status:"success"});
+      toggle();
+  } else {
+      res.json({status:"already open"});
+  }
+};
