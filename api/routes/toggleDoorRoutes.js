8727cc248f7c8bdac1896c19b1b6b3c1a9f84839
matgenois <7738711+matgenois@users.noreply.github.com>
2018-06-27 13:13:53 -0400

    Added open and close route

--- api/routes/toggleDoorRoutes.js
+++ api/routes/toggleDoorRoutes.js
@@ -7,6 +7,14 @@ module.exports = function(app) {
     app.route('/garage/door/')
         .post(garageDoor.toggle);
 
+    // todoList Routes
+    app.route('/garage/door/open')
+        .post(garageDoor.open);
+
+    // todoList Routes
+    app.route('/garage/door/close')
+        .post(garageDoor.close);
+
 
     app.route('/garage/door/')
         .get(garageDoor.readState);
